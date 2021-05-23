c8ee78efbfeaa2d0bd52bc74e89333674845060e
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-05-23 19:49:40 +0900

    fix: 🐛 NOT NULL 制約は、JSON レスポンスからの代入を経た場合は無効なのでいったん全コメントアウト (#52)
    * fix: 🐛 NOT NULL 制約は、JSON レスポンスからの代入を経た場合は無効なのでいったん全コメントアウト
    
    * fix: 🐛 テストを併せて修正した

--- db/migrate/20210522033709_create_messages.rb
+++ db/migrate/20210522033709_create_messages.rb
@@ -1,13 +1,13 @@
 class CreateMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :messages do |t|
-      t.bigint :id_number, null: false
-      t.string :type_name, null: false, default: '_TYPE_NAME_IS_NOTHING_'
+      t.bigint :id_number
+      t.string :type_name # , null: false, default: '_TYPE_NAME_IS_NOTHING_'
       t.datetime :timestamp
       t.datetime :timestamp_edited
       t.datetime :call_ended_timestamp
-      t.boolean :is_pinned, null: false, default: false
-      t.string :content, null: false, default: '_CONTENT_IS_NOTHING_'
+      t.boolean :is_pinned # , null: false, default: false
+      t.string :content # , null: false, default: '_CONTENT_IS_NOTHING_'
 
       t.references :user
       t.references :channel
