295412565fae2d8c6cdb4882ec6e83128b1f8e5f
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-05-23 11:58:14 +0900

    fix: 🐛 datetime型のカラムはdefaultの設定タイミングを考慮し、NOT NULL を外した (#45)

--- db/migrate/20210522033709_create_messages.rb
+++ db/migrate/20210522033709_create_messages.rb
@@ -3,9 +3,9 @@ class CreateMessages < ActiveRecord::Migration[6.1]
     create_table :messages do |t|
       t.bigint :id_number, null: false
       t.string :type_name, null: false, default: '_TYPE_NAME_IS_NOTHING_'
-      t.datetime :timestamp, null: false, default: Time.zone.parse('1980/01/01 12:00:00')
-      t.datetime :timestamp_edited, null: false, default: Time.zone.parse('1980/01/01 12:00:00')
-      t.datetime :call_ended_timestamp, null: false, default: Time.zone.parse('1980/01/01 12:00:00')
+      t.datetime :timestamp
+      t.datetime :timestamp_edited
+      t.datetime :call_ended_timestamp
       t.boolean :is_pinned, null: false, default: false
       t.string :content, null: false, default: '_CONTENT_IS_NOTHING_'
 
