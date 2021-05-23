33d05e4ff7e114ff04e78afc289a7b5031802d43
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-05-23 11:19:30 +0900

    fix: 🐛 カラム名を受信 JSON の key と一致させるように修正した (#44)

--- db/migrate/20210522033709_create_messages.rb
+++ db/migrate/20210522033709_create_messages.rb
@@ -5,7 +5,7 @@ class CreateMessages < ActiveRecord::Migration[6.1]
       t.string :type_name, null: false, default: '_TYPE_NAME_IS_NOTHING_'
       t.datetime :timestamp, null: false, default: Time.zone.parse('1980/01/01 12:00:00')
       t.datetime :timestamp_edited, null: false, default: Time.zone.parse('1980/01/01 12:00:00')
-      t.datetime :call_end_timestamp, null: false, default: Time.zone.parse('1980/01/01 12:00:00')
+      t.datetime :call_ended_timestamp, null: false, default: Time.zone.parse('1980/01/01 12:00:00')
       t.boolean :is_pinned, null: false, default: false
       t.string :content, null: false, default: '_CONTENT_IS_NOTHING_'
 
