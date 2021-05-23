b94894a0e187258b9bcdf77ece16e9556563b279
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-05-23 11:57:44 +0900

    fix: 🐛 datetime型のカラムはdefaultの設定タイミングを考慮し、NOT NULL を外した

--- db/migrate/20210522033855_create_embeds.rb
+++ db/migrate/20210522033855_create_embeds.rb
@@ -3,7 +3,7 @@ class CreateEmbeds < ActiveRecord::Migration[6.1]
     create_table :embeds do |t|
       t.string :title, null: false, default: '_TITLE_IS_NOTHING_'
       t.string :url, null: false, default: '_URL_IS_NOTHING_'
-      t.datetime :timestamp, null: false, default: Time.zone.parse('1980/01/01 12:00:00')
+      t.datetime :timestamp
       t.string :description, null: false, default: '_DESCRIPTION_IS_NOTHING_'
       t.jsonb :thumbnail
       t.string :fields, null: false, default: '_FIELDS_IS_NOTHING_' # レスポンスは Array だが、レスポンス例が分からないのでとりあえず String 型で格納する
